178a5487f4815db5bc917b72db71b5c86ef10f00
nishan <nishanpradhan500@gmail.com>
2025-11-01 20:42:59 +0545

    swap page

--- lib/features/learner/swap/screen/learner_swap_screen.dart
+++ lib/features/learner/swap/screen/learner_swap_screen.dart
@@ -5,9 +5,37 @@ class LearnerSwapScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Swap Screen', style: TextTheme.of(context).headlineLarge),
+    return DefaultTabController(
+      length: 3, // Number of tabs
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('My Swaps'),
+          scrolledUnderElevation: 0,
+          elevation: 0,
+          centerTitle: false,
+          bottom: TabBar(
+            isScrollable: true,
+            splashBorderRadius: BorderRadius.all(Radius.circular(8)),
+            tabAlignment: TabAlignment.start,
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            labelPadding: EdgeInsets.symmetric(horizontal: 15),
+            dividerColor: Theme.of(context).colorScheme.surface,
+            labelColor: Theme.of(context).colorScheme.primary,
+            unselectedLabelColor: Colors.grey,
+            tabs: [
+              Tab(text: 'Pending'),
+              Tab(text: 'Active'),
+              Tab(text: 'Completed'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Center(child: Text('Pending Swaps')),
+            Center(child: Text('Active Swaps')),
+            Center(child: Text('Completed Swaps')),
+          ],
+        ),
       ),
     );
   }
