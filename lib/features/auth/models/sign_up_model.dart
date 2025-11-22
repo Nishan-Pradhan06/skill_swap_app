b5aa4346d079acbd9478da91c34afa20c249ffe2
nishan <nishanpradhan500@gmail.com>
2025-11-22 12:10:19 +0545

    model and repo for signup

---
+++ lib/features/auth/models/sign_up_model.dart
@@ -0,0 +1,19 @@
+
+class SignUpModel {
+  final String email;
+  final String password;
+  final String confirmPassword;
+  SignUpModel({
+    required this.email,
+    required this.password,
+    required this.confirmPassword,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'email': email,
+      'password': password,
+      'confirmPassword': confirmPassword,
+    };
+  }
+}
--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -4,9 +4,11 @@ import 'package:skill_swap/core/network/api_services.dart';
 
 import '../../../core/services/cache_service.dart';
 import '../models/sign_in_model.dart';
+import '../models/sign_up_model.dart';
 
 abstract interface class AuthRepository {
   FutureEither<String> signIn({required SignInModel signInModel});
+  FutureEither<String> signUp({required SignUpModel signUpModel});
 }
 
 class AuthRepositoryImpl implements AuthRepository {
@@ -19,6 +21,20 @@ class AuthRepositoryImpl implements AuthRepository {
     final response = await _apiService.post<Map>(
       'auth/login/',
       data: {...signInModel.toMap()},
+    );
+
+    return response.fold((failure) => Left(failure), (data) async {
+      final token = data['token']['access'];
+      await CacheServices.instance.setAuthToken(token);
+      return Right(token);
+    });
+  }
+
+  @override
+  FutureEither<String> signUp({required SignUpModel signUpModel}) async {
+    final response = await _apiService.post<Map>(
+      'auth/signup/',
+      data: {...signUpModel.toMap()},
     );
 
     return response.fold((failure) => Left(failure), (data) async {
