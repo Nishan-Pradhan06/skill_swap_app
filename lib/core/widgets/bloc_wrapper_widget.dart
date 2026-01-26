fe0dfff6c9f18b9908d6d6151eb3bbfeec3da797
Nishan-Pradhan06 <nishanpradhan500@gmail.com>
2026-01-26 09:46:35 +0545

    interceptor, bloc

--- lib/core/widgets/bloc_wrapper_widget.dart
+++ lib/core/widgets/bloc_wrapper_widget.dart
@@ -15,6 +15,14 @@ import '../../features/profile/bloc/get_profile/get_profile_bloc.dart';
 import '../../features/profile/bloc/profile_setup/profile_setup_bloc.dart';
 import '../../features/profile/cubit/theme_appearance_cubit.dart';
 import '../../features/shared/on_boarding/cubit/on_boarding_cubit.dart';
+import '../../features/skill_swap/blocs/availability_bloc.dart';
+import '../../features/skill_swap/blocs/booking_bloc.dart';
+import '../../features/skill_swap/blocs/get_skill_swap_posts_bloc.dart';
+import '../../features/skill_swap/blocs/manage_skill_post_bloc.dart';
+import '../../features/skill_swap/blocs/get_mentor_posts_bloc.dart';
+import '../../features/skill_swap/blocs/get_categories_bloc.dart';
+import '../../features/skill_swap/blocs/get_sessions_bloc.dart';
+import '../../features/skill_swap/blocs/handle_session_action_bloc.dart';
 import '../di/dependency_injection.dart';
 import '../services/once_cache_service.dart';
 
@@ -34,14 +42,33 @@ class BlocWrapperWidget extends StatelessWidget {
         BlocProvider(create: (_) => sl<SignInBloc>()),
         BlocProvider(create: (_) => sl<SignUpBloc>()),
         BlocProvider(create: (_) => sl<ProfileSetupBloc>()),
-        BlocProvider(create: (_) => sl<GetProfileBloc>()..add(GetProfileEvent.getProfile())),
+        BlocProvider(
+          create: (_) =>
+              sl<GetProfileBloc>()..add(GetProfileEvent.getProfile()),
+        ),
         BlocProvider(create: (_) => sl<SignOutBloc>()),
-        BlocProvider(create: (_) => sl<GetNotificationBloc>()..add(GetNotificationEvent.getNotification())),
+        BlocProvider(
+          create: (_) =>
+              sl<GetNotificationBloc>()
+                ..add(GetNotificationEvent.getNotification()),
+        ),
         BlocProvider(create: (_) => sl<DeviceRegisterBloc>()),
         BlocProvider(create: (_) => sl<ReadNotificationsBloc>()),
         BlocProvider(create: (_) => sl<DeleteNotificationBloc>()),
-        BlocProvider(create: (_) => sl<GetNotificationCountBloc>()..add(GetNotificationCountEvent.getNotificationCount())),
+        BlocProvider(
+          create: (_) =>
+              sl<GetNotificationCountBloc>()
+                ..add(GetNotificationCountEvent.getNotificationCount()),
+        ),
         BlocProvider(create: (_) => sl<SwitchRoleBloc>()),
+        BlocProvider(create: (_) => sl<GetSkillSwapPostsBloc>()),
+        BlocProvider(create: (_) => sl<AvailabilityBloc>()),
+        BlocProvider(create: (_) => sl<BookingBloc>()),
+        BlocProvider(create: (_) => sl<ManageSkillPostBloc>()),
+        BlocProvider(create: (_) => sl<GetMentorPostsBloc>()),
+        BlocProvider(create: (_) => sl<GetCategoriesBloc>()),
+        BlocProvider(create: (_) => sl<GetSessionsBloc>()),
+        BlocProvider(create: (_) => sl<HandleSessionActionBloc>()),
         BlocProvider(
           create: (_) =>
               sl<DailyRewardBloc>()..add(DailyRewardEvent.dailyReward()),
